2788124c38591590b86c1922169c62fc9e9b6649
burakmngl <94786062+burakmngl@users.noreply.github.com>
2022-02-08 19:21:00 +0300

    Add files via upload

---
+++ 01-Class yapısı/01-Class yapısı/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace _01_Class_yapısı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.ListBox1 = new System.Windows.Forms.ListBox();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.txtNO = new System.Windows.Forms.TextBox();
+            this.txtSOYAD = new System.Windows.Forms.TextBox();
+            this.txtAD = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(13, 60);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(42, 13);
+            this.Label3.TabIndex = 17;
+            this.Label3.Text = "numara";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(13, 38);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(37, 13);
+            this.Label2.TabIndex = 16;
+            this.Label2.Text = "soyadı";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(13, 15);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(19, 13);
+            this.Label1.TabIndex = 15;
+            this.Label1.Text = "ad";
+            // 
+            // ListBox1
+            // 
+            this.ListBox1.FormattingEnabled = true;
+            this.ListBox1.Location = new System.Drawing.Point(58, 205);
+            this.ListBox1.Name = "ListBox1";
+            this.ListBox1.Size = new System.Drawing.Size(120, 95);
+            this.ListBox1.TabIndex = 14;
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(227, 237);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(75, 23);
+            this.Button2.TabIndex = 13;
+            this.Button2.Text = "Button2";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(227, 50);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 12;
+            this.Button1.Text = "Button1";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtNO
+            // 
+            this.txtNO.Location = new System.Drawing.Point(58, 57);
+            this.txtNO.Name = "txtNO";
+            this.txtNO.Size = new System.Drawing.Size(100, 20);
+            this.txtNO.TabIndex = 11;
+            // 
+            // txtSOYAD
+            // 
+            this.txtSOYAD.Location = new System.Drawing.Point(58, 35);
+            this.txtSOYAD.Name = "txtSOYAD";
+            this.txtSOYAD.Size = new System.Drawing.Size(100, 20);
+            this.txtSOYAD.TabIndex = 10;
+            // 
+            // txtAD
+            // 
+            this.txtAD.Location = new System.Drawing.Point(58, 12);
+            this.txtAD.Name = "txtAD";
+            this.txtAD.Size = new System.Drawing.Size(100, 20);
+            this.txtAD.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 337);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.ListBox1);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.txtNO);
+            this.Controls.Add(this.txtSOYAD);
+            this.Controls.Add(this.txtAD);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.ListBox ListBox1;
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.TextBox txtNO;
+        internal System.Windows.Forms.TextBox txtSOYAD;
+        internal System.Windows.Forms.TextBox txtAD;
+    }
+}
+
---
+++ 01-Class yapısı/01-Class yapısı/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_Class_yapısı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Öğrenci> ÖğrenciListesi = new List<Öğrenci>();
+         
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Öğrenci ogr1 = new Öğrenci();
+            ogr1.ad = txtAD.Text;
+            ogr1.soyadı = txtSOYAD.Text;
+            ogr1.numara = txtNO.Text;
+            ÖğrenciListesi.Add(ogr1);
+            ogr1.ekle();
+            txtAD.Text = "";
+            txtSOYAD.Text = "";
+            txtNO.Text = "";
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            ListBox1.Items.Clear();
+            foreach (Öğrenci x in ÖğrenciListesi)
+            {
+                ListBox1.Items.Add(x.numara + " " + x.ad + " " + x.soyadı);
+            }
+            int eleman = 0;
+            eleman = ÖğrenciListesi.Count();
+            ListBox1.Items.Add("eleman sayısı " + eleman);
+        }
+    }
+}
