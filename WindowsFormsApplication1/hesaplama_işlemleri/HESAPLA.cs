97ca7d2eedb29780ff907f45c30829e7d47d76c7
burakmngl <94786062+burakmngl@users.noreply.github.com>
2022-03-25 11:12:45 +0300

    Add files via upload

---
+++ WindowsFormsApplication1/hesaplama_işlemleri/HESAPLA.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hesaplama_işlemleri
+{
+    class HESAPLA
+    {
+        public void tekSayılar()
+        {
+            Console.WriteLine("lütfen bir sayı giriniz");
+            int x = int.Parse(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+                for (int i = 0; i < x; i+=2)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine(" girilen sayı ciftir");
+                
+            }
+            else
+            {
+                for (int i = 1; i < x; i += 2)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine("girilen sayı tektir");
+                
+            }
+            Console.ReadKey();
+        }
+        public void çitSayılar()
+        {
+
+        }
+    }
+}
